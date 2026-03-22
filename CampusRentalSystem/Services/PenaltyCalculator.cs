4bb71799e7705ebcd78198bf44c5ecd38532dd00
Michal Chojna <m.chojna@icloud.com>
2026-03-22 18:28:00 +0100

    feat: add custom penalty calculator

---
+++ CampusRentalSystem/Services/PenaltyCalculator.cs
@@ -0,0 +1,14 @@
+namespace CampusRentalSystem.Services;
+
+public class PenaltyCalculator
+{
+    private const double DailyPenaltyRate = 5.0;
+
+    public double CalculatePenalty(DateOnly expectedReturnDate, DateOnly actualReturnDate)
+    {
+        if (actualReturnDate <= expectedReturnDate) return 0.0;
+
+        var daysLate = actualReturnDate.DayNumber - expectedReturnDate.DayNumber;
+        return daysLate * DailyPenaltyRate;
+    }
+}
